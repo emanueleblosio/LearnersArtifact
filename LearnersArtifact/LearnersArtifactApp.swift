5aa1146cb5a0222203a448d9230470de1c19470b
emanueleblosio <92686353+emanueleblosio@users.noreply.github.com>
2022-04-04 15:58:16 +0200

    Initial Commit

---
+++ LearnersArtifact/LearnersArtifactApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnersArtifactApp.swift
+//  LearnersArtifact
+//
+//  Created by Emanuele Blosio on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnersArtifactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
